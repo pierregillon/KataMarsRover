3a19a5e3f3048ed0bfef28f5380f5a9ce54ea47d
Sonia BARBIER <dumboster@gmail.com>
2021-01-26 14:05:45 +0100

    Refacto

--- KataMarsRover/Rover.fs
+++ KataMarsRover/Rover.fs
@@ -2,42 +2,45 @@
 
 type Coordinate = { X: int; Y: int }
 
-type Command = string
-    
+type Command = 
+    | Right
+    | Left
+    | Forward
+    | Backward
+
 type Direction = 
-    | North = 1
-    | East = 2
-    | South = 3
-    | West = 4
+    | North
+    | East
+    | South
+    | West
 
 let toRight (currentDirection: Direction): Direction =
-    currentDirection 
-        |> int 
-        |> (fun x -> x + 1)
-        |> (fun x -> x % 4)
-        |> enum<Direction>
+    match currentDirection with
+    | North -> East
+    | East -> South
+    | South -> West
+    | West -> North      
 
 let toLeft (currentDirection: Direction): Direction =
-   currentDirection 
-       |> int 
-       |> (fun x -> x - 1)
-       |> (fun x -> if x = 0 then 4 else x)
-       |> enum<Direction>
+    match currentDirection with
+    | North -> West
+    | West -> South
+    | South -> East
+    | East -> North  
 
 type Rover(position: Coordinate, direction: Direction) = 
     member _.Position = position
     member _.Direction = direction
     member _.changeDirection(newDirection: Direction): Rover = Rover(position, newDirection)
     member _.changePosition(newPosition: Coordinate): Rover = Rover(newPosition, direction)
-    new() = Rover({X= 0 ; Y=0}, Direction.North)
+    new() = Rover({ X= 0 ; Y=0 }, Direction.North)
 
 let determinateNextPosition (position: Coordinate) (direction: Direction) (number: int): Coordinate =
     match direction with
-    | Direction.North -> {X= position.X ; Y=position.Y + number}
-    | Direction.East -> {X = position.X + number ; Y = position.Y}
-    | Direction.South -> {X = position.X ; Y = position.Y - number}
-    | Direction.West -> {X = position.X - number ; Y = position.Y}
-    | _ -> position
+    | North -> { position with Y = position.Y + number }
+    | East -> { position with X = position.X + number }
+    | South -> { position with Y = position.Y - number }
+    | West -> { position with X = position.X - number }
 
 let moveForward (rover: Rover): Rover =
     1 
@@ -51,33 +54,46 @@ let moveBackward (rover: Rover): Rover =
 
 let rotateLeft (rover: Rover): Rover =
     rover.Direction
-        |> toLeft 
-        |> rover.changeDirection
+    |> toLeft 
+    |> rover.changeDirection
 
 let rotateRight (rover: Rover): Rover =
     rover.Direction
-        |> toRight 
-        |> rover.changeDirection
+    |> toRight 
+    |> rover.changeDirection
 
 let execute (rover: Rover) (command: Command): Rover = 
     match command with
-    | "R" -> rover |> rotateRight
-    | "L" -> rover |> rotateLeft
-    | "F" -> rover |> moveForward
-    | "B" -> rover |> moveBackward
-    | _ -> rover
+    | Right -> rover |> rotateRight
+    | Left -> rover |> rotateLeft
+    | Forward -> rover |> moveForward
+    | Backward -> rover |> moveBackward
 
-let first (command: Command): Command =
-     command.Substring(0, 1)
+let rec executeCommandList (commands: Command list) (rover: Rover): Rover = 
+    match commands with
+    | [] -> rover
+    | head :: tail -> 
+        head
+        |> execute rover
+        |> executeCommandList tail
 
-let others (command: Command): Command =
-    command.Substring(1)
+let toCommand (command: char) : Command =
+    match command with
+    | 'R' -> Right
+    | 'L' -> Left
+    | 'F' -> Forward
+    | 'B' -> Backward
+    | _ -> failwith "Unable to convert command"
 
-let rec executeCommand (command: Command) (rover: Rover): Rover = 
-    match command.Length with
-    | 1 -> command |> first |> execute rover
-    | _ -> command
-           |> first
-           |> execute rover
-           |> executeCommand (command |> others)
-    
+let toCommands commands =
+    commands 
+    |> Seq.toList
+    |> List.map toCommand
+        
+let executeCommand (commands: string) (rover: Rover): Rover = 
+
+    let execteCommandOnRover commands = executeCommandList commands rover
+
+    commands 
+    |> toCommands
+    |> execteCommandOnRover
